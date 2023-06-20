b8a122ea057ec741527b2dec12498d2212a26cd3
luigimariapascali <luigimariapascali@gmail.com>
2023-06-20 17:39:41 +0200

    pari e dispari complete

--- PARI E DISPARI/js/script.js
+++ PARI E DISPARI/js/script.js
@@ -9,39 +9,81 @@
         ALTRIMENTI => il computer vince
 */
 
-// VERSIONE 1:
+// // VERSIONE 1:
 
-let userChoice = prompt("Scegli pari o dispari");
-if (userChoice !== "pari" && userChoice !== "dispari") {
-    alert ("Inserisci solo 'pari' o 'dispari'");
-}
+// let userChoice = prompt("Scegli pari o dispari");
+// if (userChoice !== "pari" && userChoice !== "dispari") {
+//     alert ("Inserisci solo 'pari' o 'dispari'");
+// }
 
-let userNumber = parseInt (prompt("Inserisci un numero da 1 a 5"));
-if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
-   alert ("Inserisci solo un numero da 1 a 5");
-}
+// let userNumber = parseInt (prompt("Inserisci un numero da 1 a 5"));
+// if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+//    alert ("Inserisci solo un numero da 1 a 5");
+// }
+
+// // Genero un numero random per il computer:
+
+// let cpuNumber = Math.floor(Math.random() * 5) + 1;
+
+// // Sommo i due numeri: 
+
+// let sum = userNumber + cpuNumber;
+
+// // funzione per stabilire se la somma dei due numeri è pari o dispari:
+// function isEven(number) {
+//     return number % 2 === 0;
+// }
+
+// let result = isEven(sum) ? "pari" : "dispari";
+
+// // Dichiaro il vincitore: 
+
+// if (result === userChoice) {
+//     console.log ("User ha vinto!");
+// } else {
+//     console.log("Computer ha vinto!")
+// }
 
 
-// Genero un numero random per il computer:
 
-let cpuNumber = Math.floor(Math.random() * 5) + 1;
+// VERSIONE 2:
 
-// Sommo i due numeri: 
-
-let sum = userNumber + cpuNumber;
-
-// funzione per stabilire se la somma dei due numeri è pari o dispari:
 function isEven(number) {
     return number % 2 === 0;
 }
 
-let result = isEven(sum) ? "pari" : "dispari";
+let userNumber = Math.floor(Math.random() * 5) + 1;
+let cpuNumber = Math.floor(Math.random() * 5) + 1;
 
-// Dichiararo il vincitore: 
+document.getElementById("play-button").addEventListener("click", function() {
+    let userChoice = prompt("Scegli pari o dispari");
+    if (userChoice!== "pari" && userChoice!== "dispari") {
+        alert ("Inserisci solo 'pari' o 'dispari'");
+        return;
+    }
 
-if (result === userChoice) {
-    console.log ("User ha vinto!");
-} else {
-    console.log("Computer ha vinto!")
-}
+    let userNumber = parseInt (prompt("Inserisci un numero da 1 a 5"));
+    if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+       alert ("Inserisci solo un numero da 1 a 5");
+       return;
+    }
+
+    let cpuNumber = Math.floor(Math.random() * 5) + 1;
+
+    let sum = userNumber + cpuNumber;
+    let result = isEven(sum)? "pari" : "dispari";
+
+    let winner = "";
+    if (result === "pari") {
+      winner = "Tizio Incognito";
+    } else {
+      winner = "Computer";
+    }
+
+    document.getElementById("user-choice").textContent = userChoice;
+    document.getElementById("user-number").textContent = userNumber;
+    document.getElementById("cpu-number").textContent = cpuNumber;
+    document.getElementById("winner").textContent = winner;
+});
+
 

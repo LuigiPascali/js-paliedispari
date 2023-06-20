08c6f4261ce6e734cff7a97f11a35ca19c7dfa11
luigimariapascali <luigimariapascali@gmail.com>
2023-06-20 13:48:38 +0200

    palindrome js in console: complete

--- PALINDROMO/js/script.js
+++ PALINDROMO/js/script.js
@@ -6,4 +6,21 @@
     ALTRIMENTI la parola non è palindroma, stampo "La parola non è palindroma"
 */
 
+// Chiedo all'utente di inserire una parola:
 
+let word = prompt("Inserisci una parola: ");
+
+// Creo una funzione per verificare se la parola è palindroma:
+
+function isPalindrome(word) {
+    // Confronto la parola scritta con la sua versione invertita:
+    return word === word.split("").reverse().join("");
+}
+
+// Verifica e stampa del risultato in console: 
+
+if (isPalindrome(word)) {
+    console.log(word + " è palindroma!");
+    } else { 
+    console.log(word + " non è palindroma!");
+}

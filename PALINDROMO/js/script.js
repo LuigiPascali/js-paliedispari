2ed4506e1743447aceea1c625bb7d4ab001e68ca
luigimariapascali <luigimariapascali@gmail.com>
2023-06-20 13:32:14 +0200

    Scritto in forma testuale js palindrome

---
+++ PALINDROMO/js/script.js
@@ -0,0 +1,9 @@
+/* 
+    Chiedo all'utente di inserire una parola
+    Creo una funzione per verificare se la parola è palindroma
+    Per vedere se una parola è palindroma confronto la parola scritta con la sua versione invertita
+    SE la parola è palindroma, stampo "La parola è palindroma"
+    ALTRIMENTI la parola non è palindroma, stampo "La parola non è palindroma"
+*/
+
+
